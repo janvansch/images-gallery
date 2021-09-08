1d6969e9ca74a7bc1676d1857d9e92622da0ec01
janvansch <jvs@nimbledesign.co.za>
2021-09-08 19:22:27 +0200

    First API Call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header'; // no {} as this is a default export
 import Search from './components/Search';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
 const App = () => {
   const [word, setWord] = useState(''); 
     // useState returns a stateful value and 
@@ -10,10 +12,19 @@ const App = () => {
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(word)
+    console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      }
+    )
   }
 
-  console.log(word);
+  console.log(process.env);
 
   return (
     <div>
