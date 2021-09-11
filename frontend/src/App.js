02d4817630ee854e6aa1e9f834c8886db934a713
janvansch <jvs@nimbledesign.co.za>
2021-09-11 15:04:10 +0200

    display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,19 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header'; // no {} as this is a default export
 import Search from './components/Search';
+import ImageCard from './components/ImageCard';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
-  const [word, setWord] = useState('');
-  // useState returns a stateful value and
+  // useState() returns a stateful value and
   // a function to update the stateful value
+  const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-
-    console.log(word);
 
     fetch(
       `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
@@ -21,7 +22,7 @@ const App = () => {
       .then((res) => res.json()) // call utility function
       .then((data) => {
         // call defined function
-        console.log(data);
+        setImages([{ ...data, title: word }, ...images]); // Adding title = word to data object
         setWord('');
       })
       .catch((err) => {
@@ -29,12 +30,25 @@ const App = () => {
       });
   };
 
-  console.log(process.env);
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => image.id !== id)); // implicit return
+    // Filter returns new array
+  };
 
   return (
     <div>
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+            // Arrow function with implicit return of an array of image cards with key = index i
+            <Col key={i} className="pb-3">
+              <ImageCard image={image} deleteImage={handleDeleteImage} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
