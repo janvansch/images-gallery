b177b6461df49baa3544134e0f32a4fc4a37d167
janvansch <jvs@nimbledesign.co.za>
2021-10-05 16:25:35 +0200

    Basic images endpoint with GET and POST methods

--- api/main.py
+++ api/main.py
@@ -1,8 +1,12 @@
 import os
 import requests
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from flask_cors import CORS
+from mongo_client import mongo_client
+
+gallery = mongo_client.gallery  # The database
+images_collection = gallery.images  # The collection in the database
 
 load_dotenv(dotenv_path="./.env.local")
 
@@ -24,10 +28,10 @@ app.config["DEBUG"] = True if MODE == "dev" else False
 
 @app.route("/")
 def hello():
-    return "From Flask on RPi: Hello, World!"
+    return "Hello from Flask!"
 
 
-@app.route("/new-image")  # only GET is allowed
+@app.route("/new-image")  # Only GET is allowed
 def new_image():
     word = request.args.get("query")
     headers = {"Accept-Version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY}
@@ -35,6 +39,23 @@ def new_image():
     response = requests.get(url=UNSPLASH_URL, params=params, headers=headers)
     data = response.json()
     return data
+
+
+@app.route("/images", methods=["GET", "POST"])  # Only GET and POST are allowed
+def images():
+    if request.method == "GET":
+        # Read images from DB
+        images = images_collection.find({})  # Returns a cursor
+        return jsonify(
+            [img for img in images]
+        )  # Traverse cursor and create img list and then return it in JSON format
+    if request.method == "POST":
+        # Save image to DB
+        image = request.get_json()
+        image["_id"] = image.get("id")
+        result = images_collection.insert_one(image)
+        inserted_id = result.inserted_id
+        return {"inserted_id": inserted_id}
 
 
 if __name__ == "__main__":
